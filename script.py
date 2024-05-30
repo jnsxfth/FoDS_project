9dd8048f96dbe6d9200b3485269579bc8f116ca3
jnsxfth <jfreiermuth@student.ethz.ch>
2024-05-30 11:18:54 +0200

    Update

--- script.py
+++ script.py
@@ -136,8 +136,8 @@ def plot_data(data, column):
     plt.figure(figsize=(10, 8))
     sns.histplot(data[column], binwidth=1, discrete=True, color='#008F91')
     plt.title(f"Distribution of\n{titles[column]} in {get_dataset_name(data)}", size=14, fontweight='bold')
-    plt.xlabel(xlabels[column], fontweight='bold', size=14)
-    plt.ylabel('Count', fontweight='bold', size=14)
+    plt.xlabel(xlabels[column], fontweight='bold', size=16)
+    plt.ylabel('Count', fontweight='bold', size=16)
     plt.savefig(f'./output/{get_dataset_name(data)}/{column}_histplot')
     plt.close()
 
@@ -147,8 +147,8 @@ def boxplot_data(data, column):
     plt.figure(figsize=(10, 8))
     sns.boxplot(x=data[column], color='#008F91')
     plt.title(f'Distribution of\n {titles[column]} in {get_dataset_name(data)}', size=14, fontweight='bold')
-    plt.xlabel(xlabels[column], fontweight='bold', size=14)
-    plt.ylabel('Count', fontweight='bold', size=14)
+    plt.xlabel(xlabels[column], fontweight='bold', size=16)
+    plt.ylabel('Count', fontweight='bold', size=16)
     plt.savefig(f'./output/{get_dataset_name(data)}/{column}_boxplot')
     plt.close()
 
